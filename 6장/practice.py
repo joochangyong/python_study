feda69d8d99dda0247141752fd1b943f245bfb7b
joochangyong <wnckddyd0525@gmail.com>
2021-01-12 23:56:06 +0900

    6장

---
+++ 6장/practice.py
@@ -0,0 +1,98 @@
+##### if문
+'''
+if 조건:
+    실행문
+elif 조건:
+    실행문
+else:
+    실행문
+'''
+
+'''
+weather = input("오늘 날씨는 어때요?")
+
+if weather == "비" or weather == "눈":
+    print("우산을 챙기세요")
+elif weather == "미세먼지":
+    print("마스크를 챙기세요")
+else:
+    print("준비물 필요 없어요")
+
+
+temp = int(input("기온은 어때요?")) # 기온은 숫자이기때문에 int로 바꿔준다.
+if temp >= 30:
+    print("너무 더워요. 나가지마세요")
+elif temp >= 10 and temp < 30:
+    print("괜찮은 날씨에요")
+elif 0 <= temp < 10:
+    print("추워요. 외투를 챙기세요")
+else:
+    print("너무 추워요. 나가지 마세요")
+'''
+
+##### for문
+'''
+for waiting_no in range(1, 6):
+    print("대기번호 : {0}" .format(waiting_no))
+
+starbucks = ["아이언맨", "토르", "아이엠 그루트"]
+for customer in starbucks:
+    print("{0}, 커피가 준비되었습니다." .format(customer))
+'''
+
+##### while문
+'''
+customer = "토르"
+index = 5
+while index >= 1:
+    print("{0}, 손님 커피가 준비 되었습니다. {1}번 남았어요." .format(customer, index))
+    index -= 1
+    if index == 0:
+        print("커피는 폐기처분 되었습니다.")
+
+# 무한루프
+customer = "아이언맨"
+index = 1
+while True:
+    print("{0}, 손님 커피가 준비 되었습니다. 호출 {1}회" .format(customer, index))
+    index += 1
+
+# 중간에 종료
+customer = "토르"
+person = "Unknown"
+while person != customer:
+    print("{0}, 손님 커피가 준비 되었습니다." .format(customer))
+    person = input("이름이 어떻게 되세요?")
+'''
+
+##### continue와 break
+'''
+absent = [2, 5] # 결석
+no_book = [7] # 책을 안들고옴
+for student in range(1, 11):
+    if student in absent:
+        continue
+    elif student in no_book:
+        print("오늘 수업 여기까지. {0}은 교무실로 따라와" .format(student))
+        break
+    print("{0}번아 책을 읽어봐" .format(student))
+'''
+
+##### 한줄 for
+'''
+# 출석번호가 1 2 3 4, 앞에 100을 붙이기로함 -> 101, 102, 103, 104
+students = [1, 2, 3, 4, 5]
+print(students)
+students = [i + 100 for i in students]
+print(students)
+
+# 학생 이름을 길이로 변환
+students = ["IronMan", "Thor", "I am groot"]
+students = [len(i) for i in students]
+print(students)
+
+# 학생 이름을 대문자로 변환
+students = ["IronMan", "Thor", "I am groot"]
+students = [i.upper() for i in students]
+print(students)
+'''
